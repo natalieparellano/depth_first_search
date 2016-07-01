61c9186f8c2c07ecb9d7b8180a1cb5036bc5c828
Natalie Arellano <natalie.p.arellano@gmail.com>
2016-07-01 14:22:13 -0400

    Re-write to pass in pointer as an argument

--- lib/searcher.rb
+++ lib/searcher.rb
@@ -9,15 +9,29 @@ class Searcher
   end
   def search( value )
     @visited = [] # to make it idempotent
-    @pointer = tree.head
-    do_search( value )
-    if pointer.value == value
-      return pointer
-    else
-      return nil
-    end
+    # @pointer = tree.head
+    do_search0( value, tree.head )
+    # if pointer.value == value
+    #   return pointer
+    # else
+    #   return nil
+    # end
   end
   private
+    def do_search0( value, pointer )
+      visited << pointer.value
+      if ( pointer.value == value )
+        return pointer
+      else
+        pointer.children.each do |node|
+          return if visited.include?( value ) || # we found it through a sibling that was evaluated first
+              visited.include?( node.value ) # the tree has a cycle 
+          do_search0( value, node ) 
+        end
+        return nil # otherwise the return value when nothing is found will be pointer.children        
+      end           
+    end
+
     def do_search( value )
       # actually, this does NOT work because the pointer has been moved already! 
       #   "visited" will be correct (and pass the tests), but if we have expectations around 
